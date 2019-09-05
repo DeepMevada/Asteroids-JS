1a16015b9b30ba96201ed9dfd44d2bc90001ff88
Deep Mevada <53739044+DeepMevada@users.noreply.github.com>
2019-09-05 23:34:24 +0530

    Update asteroids.js

--- asteroids.js
+++ asteroids.js
@@ -156,7 +156,7 @@ class Asteroid {
         this.visible = true;
         this.x = x || Math.floor(Math.random() * canvasWidth);
         this.y = y || Math.floor(Math.random() * canvasHeight);
-        this.speed = 3;
+        this.speed = 1.5;
         this.radius = radius || 50;
         this.angle = Math.floor(Math.random() * 359);
         this.strokeColor = 'white';
